0325a0afde6d6a625dac6c3a3c9f5493514ce7fb
Hugo Zammit <hugo.zammit@signature-code.com>
2020-04-15 19:18:46 +0200

    Add about.component.

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,22 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NzGridModule } from 'ng-zorro-antd/grid';
 import { NzIconModule } from 'ng-zorro-antd/icon';
 
 import { UiModule } from '../ui/ui.module';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { HeroAreaComponent } from './components/hero-area/hero-area.component';
+import { AboutComponent } from './components/about/about.component';
 import { AboutCardComponent } from './components/about-card/about-card.component';
 
 @NgModule({
   declarations: [
     HomePageComponent,
     HeroAreaComponent,
+    AboutComponent,
     AboutCardComponent,
   ],
   imports: [
     CommonModule,
     UiModule,
+    NzGridModule,
     NzIconModule,
     HomeRoutingModule
   ]
