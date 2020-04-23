fed42d6a00333f4a435a0ae63dc19069768cce33
Hugo Zammit <hugo.zammit@signature-code.com>
2020-04-23 22:43:09 +0200

    Add blog-page.component.

--- src/app/blog/blog.module.ts
+++ src/app/blog/blog.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { BlogPostComponent } from './components/blog-post/blog-post.component';
 import { BlogPageComponent } from './pages/blog-page/blog-page.component';
 import { BlogRoutingModule } from './blog-routing.module';
+import { UiModule } from '../ui/ui.module';
 
 @NgModule({
   imports: [
     CommonModule,
+    UiModule,
     BlogRoutingModule
   ],
   declarations: [
--- src/app/blog/pages/blog-page/blog-page.component.ts
+++ src/app/blog/pages/blog-page/blog-page.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { blogPostRepository } from 'src/app/core/data/blog-post-repository';
+import { BlogPostView } from 'src/app/core/models/blog-post.models';
+import { NavbarItem } from 'src/app/ui/navbar/navbar.component';
 
 @Component({
   selector: 'app-blog-page',
@@ -7,9 +11,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BlogPageComponent implements OnInit {
 
-  constructor() { }
+  public post: BlogPostView;
 
-  ngOnInit() {
+  public get navigationTargets(): NavbarItem[] {
+    return [
+      {
+        label: 'Accueil',
+        navigationTarget: { routerLink: '/home', fragment: 'hero' }
+      },
+      {
+        label: 'À Propos',
+        navigationTarget: { routerLink: '/home', fragment: 'about' }
+      },
+      {
+        label: 'Compétences',
+        navigationTarget: { routerLink: '/home', fragment: 'skills' }
+      },
+      {
+        label: 'Réalisation',
+        navigationTarget: { routerLink: '/home', fragment: 'blog-posts' }
+      }
+    ]
+  }
+
+  constructor(
+    private activatedRoute: ActivatedRoute
+  ) { }
+
+  async ngOnInit() {
+    const url = this.activatedRoute.snapshot.url;
+    const lastSegment = url[url.length - 1];
+    this.post = blogPostRepository[lastSegment.path];
   }
 
 }
