513c6356fe902935ff4465234936d24d7d0c919d
Hugo Zammit <hugo.zammit@signature-code.com>
2020-06-30 00:34:26 +0200

    Add blog-post-recommendation-provider.

---
+++ src/data/models/blog-post-recommendation-provider.ts
@@ -0,0 +1,28 @@
+import { IBlogPostRecommendationsProvider, BlogPost } from "src/app/core/models/blog-post.models";
+import { BlogPosts } from '../blog-posts';
+
+class TagBasedRecommendations implements IBlogPostRecommendationsProvider {
+
+    constructor(
+        private readonly _references: ReadonlyArray<BlogPost>
+    ) { }
+
+    getRecommendations(subject: BlogPost, count: number): ReadonlyArray<BlogPost> {
+        if( !subject ) throw new Error('subject must be a valid blog post');
+        if( count < 1 ) return [];
+
+        return this._references
+            .map(reference => {
+                const weight = reference.tags
+                    .filter(tag => subject.tags.includes(tag))
+                    .length;
+                return { reference, weight };
+            })
+            .sort((a, b) => a.weight - b.weight)
+            .slice(0, count)
+            .map(tuple => tuple.reference);
+    }
+
+}
+
+export const BlogPostRecommendationsProvider = new TagBasedRecommendations(BlogPosts);
--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,9 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
-import { BLOG_POST_REPOSITORY } from './app/core/config/injection-tokens';
+import { BLOG_POST_REPOSITORY, BLOG_POST_RECOMMENDATIONS } from './app/core/config/injection-tokens';
 import { BlogPostRepository } from './data/models/blog-post-repository';
+import { BlogPostRecommendationsProvider } from './data/models/blog-post-recommendation-provider';
 
 if (environment.production) {
   enableProdMode();
@@ -16,6 +17,10 @@ platformBrowserDynamic(
     {
       provide: BLOG_POST_REPOSITORY,
       useValue: BlogPostRepository
+    },
+    {
+      provide: BLOG_POST_RECOMMENDATIONS,
+      useValue: BlogPostRecommendationsProvider
     }
   ])
   .bootstrapModule(AppModule)
