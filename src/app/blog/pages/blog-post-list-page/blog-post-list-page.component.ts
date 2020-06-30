2cb0b6c5c6cd8e357bbca902585fe6cc281d6ad8
Hugo Zammit <hugo.zammit@signature-code.com>
2020-06-30 02:45:57 +0200

    [FIX] Refresh breadcrumb on category change.

--- src/app/blog/pages/blog-post-list-page/blog-post-list-page.component.ts
+++ src/app/blog/pages/blog-post-list-page/blog-post-list-page.component.ts
@@ -14,7 +14,6 @@ import { WellKnownBlogPostCategory, BlogPostCategoryStyling } from 'src/data/blo
 })
 export class BlogPostListPageComponent implements OnInit, OnDestroy {
 
-  private _category: string;
   private _onDestroy: Subject<void>;
 
   public category: BlogPostCategory;
@@ -46,13 +45,13 @@ export class BlogPostListPageComponent implements OnInit, OnDestroy {
       (Object.values(WellKnownBlogPostCategory) as BlogPostCategory[])
         .map<NavbarItemConfiguration>(category => ({
           key: category.label as unknown as NavbarItemLabelUnion,
-          configuration: (item) => item.selected = category.path === this._category
+          configuration: (item) => item.selected = category.path === this.category.path
         }))
         .concat(
           (Object.values(BlogPostCategoryStyling) as BlogPostCategory[])
             .map<NavbarItemConfiguration>(category => ({
               key: category.label as unknown as NavbarItemLabelUnion,
-              configuration: (item) => item.selected = category.path === this._category,
+              configuration: (item) => item.selected = category.path === this.category.path,
               accept: (_) => category.path === lastSegment.path
             }))
         )
@@ -75,16 +74,17 @@ export class BlogPostListPageComponent implements OnInit, OnDestroy {
         takeUntil(this._onDestroy),
         withLatestFrom(tagQuery$),
         switchMap(([params, tag]: [ParamMap, string]) => {
-          this._category = params.get('category');
+          const categoryPath = params.get('category');
+          this.category = this.blogPostRepository.getCategoryFromPath(categoryPath);
           this.tags = !!tag ? [tag] : [];
-          return of(this.blogPostRepository.getInCategoryWithTags(this._category, this.tags));
+          return of(this.blogPostRepository.getInCategoryWithTags(this.category, this.tags));
         }))
       .subscribe(posts => this.posts = posts);
   }
 
   public tagsChange(tags: string[]) {
     this.tags = tags;
-    this.posts = this.blogPostRepository.getInCategoryWithTags(this._category, this.tags);
+    this.posts = this.blogPostRepository.getInCategoryWithTags(this.category, this.tags);
   }
 
   public ngOnDestroy() {
