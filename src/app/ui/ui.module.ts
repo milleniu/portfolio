29d0a1d673a6210406596c15ed2b900df08aaf8b
Hugo Zammit <hugo.zammit@signature-code.com>
2020-07-09 02:02:36 +0200

    Add custom svg icon provider for ng-zorro.

--- src/app/ui/ui.module.ts
+++ src/app/ui/ui.module.ts
@@ -4,11 +4,12 @@ import { RouterModule } from '@angular/router';
 
 import { NzMenuModule } from 'ng-zorro-antd/menu';
 import { NzButtonModule } from 'ng-zorro-antd/button';
-import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzIconModule, NzIconService } from 'ng-zorro-antd/icon';
 import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
 
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { SectionHeaderComponent } from './components/section-header/section-header.component';
+import { CustomIcons, getCustomIconSVG } from './shared/custom-icons/custom-icon-repository';
 
 @NgModule({
   declarations: [
@@ -28,4 +29,14 @@ import { SectionHeaderComponent } from './components/section-header/section-head
     SectionHeaderComponent
   ]
 })
-export class UiModule { }
+export class UiModule {
+  constructor(iconService: NzIconService) {
+    const customIcons = Object
+      .keys(CustomIcons)
+      .map(key => CustomIcons[key] as CustomIcons);
+
+    for(const icon of customIcons) {
+      iconService.addIconLiteral(icon, getCustomIconSVG(icon));
+    }
+  }
+}
